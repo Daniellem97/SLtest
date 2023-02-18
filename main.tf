0baf7769699d5afc108c1e6964d070679113514a
Danielle <daniellem@Danielles-MacBook-Pro.local>
2023-02-18 17:41:36 +0000

    kim instance

--- main.tf
+++ main.tf
@@ -21,3 +21,13 @@ module "danielle-dev" {
   key_name          = "danielle_key"
   instance_type     = "t2.micro"
 }
+
+module "kim-dev" {
+  source            = "./compute"
+  security_group_id = [module.networking.security_group_id]
+  subnet_id         = module.networking.subnet_id
+  host_os           = var.host_os
+  node_name         = "kim"
+  key_name          = "kim_key"
+  instance_type     = "t2.micro"
+}
